d6fcd7f691180b868b727f7854b6660a903a6e5c
yaahor <yaahor@gmail.com>
2025-03-16 18:36:46 +0100

    frontend: login: generate user list component

---
+++ frontend/src/user-list/user-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserListComponent {
+
+}
