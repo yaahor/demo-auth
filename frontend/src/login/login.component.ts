527a3de8bf3736dc55374794cb66aae969608f80
yaahor <yaahor@gmail.com>
2025-03-16 18:24:23 +0100

    frontend: login: create form layout

--- frontend/src/login/login.component.ts
+++ frontend/src/login/login.component.ts
@@ -1,12 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'app-login'
+  },
 })
 export class LoginComponent {
-
+  protected readonly formGroup = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+  });
 }
