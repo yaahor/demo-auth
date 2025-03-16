2dd6e9ecbbcb71f27e76039b25f446e033c8cde8
yaahor <yaahor@gmail.com>
2025-03-16 18:40:05 +0100

    frontend: app: add route to user list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from '../auth/auth-guard';
 import { LoginComponent } from '../login/login.component';
+import { UserListComponent } from '../user-list/user-list.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'users', component: UserListComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
 ];
