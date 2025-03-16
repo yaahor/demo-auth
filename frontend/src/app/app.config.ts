fafe3189cebd8b83fce4aae22aaf10e61deb2548
yaahor <yaahor@gmail.com>
2025-03-16 18:17:18 +0100

    frontend: deps: configure primeNg

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { providePrimeNG } from 'primeng/config';
 import { authInterceptor } from '../auth/auth-interceptor';
-
+import Aura from '@primeng/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +12,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([authInterceptor]),
-    )
+    ),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      }
+    })
   ]
 };
