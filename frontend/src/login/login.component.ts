6a014c7eb3b10b6cac5aca94b7aa9b4c4303d62f
yaahor <yaahor@gmail.com>
2025-03-16 17:57:23 +0100

    frontend: auth: generate login component

---
+++ frontend/src/login/login.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+
+}
