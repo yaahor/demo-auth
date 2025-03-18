7444764751192f334701992183ea2ee665e94da7
yaahor <yaahor@gmail.com>
2025-03-18 11:00:44 +0100

    frontend: user list: hide edit user button for regular users

--- frontend/src/pages/user-list/user-list.service.ts
+++ frontend/src/pages/user-list/user-list.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { catchError, combineLatest, map, Observable, of, startWith } from 'rxjs';
 import { UserRole } from '../../enitities/user/model/user-role';
 import { UserUpdateModel } from '../../enitities/user/model/user-update.model';
 import { UserService } from '../../enitities/user/model/user.service';
+import { AuthService } from '../../features/auth/auth.service';
 import { UserListVo } from './user-list.vo';
 
 @Injectable({
@@ -10,18 +11,25 @@ import { UserListVo } from './user-list.vo';
 })
 export class UserListService {
 
-  constructor(private readonly userService: UserService) {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService
+  ) {
   }
 
   getUserList(): Observable<UserListVo> {
     const roles = [UserRole.ADMIN, UserRole.REGULAR];
 
-    return this.userService.getUserList()
+    return combineLatest([this.userService.getUserList(), this.authService.observeCurrentUser()])
       .pipe(
-        map((userList): UserListVo => {
-          return { items: userList.items, roles };
+        map(([userList, currentUser]): UserListVo => {
+          return {
+            canEditUsers: currentUser?.role === UserRole.ADMIN,
+            items: userList.items,
+            roles,
+          };
         }),
-        catchError(() => of<UserListVo>({ roles })),
+        catchError(() => of<UserListVo>({ canEditUsers: false, roles })),
       );
   }
 
--- frontend/src/pages/user-list/user-list.vo.ts
+++ frontend/src/pages/user-list/user-list.vo.ts
@@ -2,6 +2,7 @@ import { UserListItem } from '../../enitities/user/model/user-list-item';
 import { UserRole } from '../../enitities/user/model/user-role';
 
 export interface UserListVo {
+  canEditUsers: boolean;
   items?: UserListItem[];
   roles: UserRole[];
 }
